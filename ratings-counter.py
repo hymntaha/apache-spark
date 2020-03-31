ee961e383c1f2c4f127af85f96520b093fe496f7
taha uygun <tahauygun@gmail.com>
2020-03-30 23:59:19 -0700

    dir fix

--- ratings-counter.py
+++ ratings-counter.py
@@ -9,5 +9,6 @@ ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
+
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
